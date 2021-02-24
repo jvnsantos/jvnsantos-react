5df52809f02dc2ef61b267b1fedb5105bd570bdd
jvnsantos <jvnsantos@outlook.com>
2021-02-24 17:55:28 -0300

    repage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './Components/Header';
+import Main from './Components/Main';
 
 function App() {
   return (
-    
+    <div>
+
+
     <Header />
+    <Main> João</Main>
+    
+    </div>
   );
 }
 
---
+++ src/Components/Main/index.js
@@ -0,0 +1,7 @@
+import React, { Children } from 'react'
+
+export default function Main(){
+return(
+  <h1>{pop.Children}</h1>
+);
+}
